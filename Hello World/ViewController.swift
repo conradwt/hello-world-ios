a84866e0aa53dd74920e512716a1b74dde4e6bb7
Conrad Taylor <conradwt@gmail.com>
2019-12-24 03:22:56 -0800

    Initial commit.

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Conrad Taylor on 12/5/18.
+//  Copyright © 2018 Conrad Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
